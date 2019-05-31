b256dd04c7deb05ed11be4aa5a93c2fc91116468
Joseph Millan <https://github.com/JMillan13>
2019-05-30 20:44:31 -0400

    ?

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "ajmillan13",
-  database: "burgers_db"
+  user: "gl68zf4oj42ef43x",
+  password: "hd2ahjhko85ysf7q",
+  database: "er1lafzhaaol9p4n"
 });
 
 //try to connect to mySQL
